a2abcf60fbe0ab8d324589502d2718b4082ba193
VIBINTP <118337078+VIBINTP@users.noreply.github.com>
2022-11-28 20:17:09 +0530

    Create Sum.java
    java program

---
+++ Sum.java
@@ -0,0 +1,18 @@
+public class Sum{
+public static void main(String arg[])
+ {
+  int n1=100,n2=200,n3=525,n4=5,n5=50;
+  int sum=n1+n2+n3-n4-n5;
+  System.out.println("the sum is "+sum);
+    
+
+
+}
+
+
+
+
+
+
+
+}
